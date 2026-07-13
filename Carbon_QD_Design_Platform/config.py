e12df05436ccf59f9716eed50f3060e5b32da46f
Umar Asif <umarasif2026@gmail.com>
2026-07-14 02:07:39 +0500

    Add files via upload

---
+++ Carbon_QD_Design_Platform/config.py
@@ -0,0 +1,11 @@
+# ============================================================
+# Configuration File
+# ============================================================
+
+MODEL_PATH = "models/QY_CatBoost_Model.pkl"
+
+APP_TITLE = "Carbon Quantum Dot Design Platform"
+
+PAGE_ICON = "🧪"
+
+LAYOUT = "wide"
